8b9021d757fabcb85fc9215e208c769bd801a53e
cong111-aa <ncong170206@gmail.com>
2025-11-26 15:38:05 +0700

    up list

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,13 +1,18 @@
-// Giả sử thầy yêu cầu các em tạo một App với lời chào
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import About from './pages/About'
+import Home from './pages/home'
+
+
 function App() {
-  const greeting = "Chào mừng đến với React!";
-
   return (
-    <div className="container">
-      <h1>{greeting}</h1>
-      <p>Đây là component đầu tiên của bạn</p>
-    </div>
-  );
-}
 
-export default App;
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+export default App
